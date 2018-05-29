1f8826b337d115a94df0551fddb616d854022217
Navezan <38084111+Navezan@users.noreply.github.com>
2018-05-28 20:40:18 -0700

    Updated js files #2

---
+++ js/redesign.js
@@ -0,0 +1,13 @@
+
+///Lightbox Javascript///
+$(document).foundation();
+
+///Navigation javascript///
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+  }
